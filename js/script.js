71410fe1727059679665f375e822e57851e314ec
Edgar <EdgarEGarcia3@gmail.com>
2023-11-26 20:14:57 -0600

    add getPlayerSelection function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
+function getPlayerChoice() {
+    let validPlayerInput = false;
+    
+    while (validPlayerInput == false) {
+        let playerInput = prompt("Throw your choice!")
+        let playerSelection = playerInput.toLowerCase();
+        if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
+            validPlayerInput = true;
+        } else {
+            console.log("Please throw a valid choice");
+        }
+    }
+    console.log(playerSelection);
+    return playerSelection;
+}
+
+
 function getComputerChoice() {
     let number = Math.floor(Math.random()*3);
-    let choice;
+    let computerSelection;
     if (number == 0){
-        choice = "rock";
+        computerSelection = "rock";
     } else if (number == 1) {
-        choice = "paper";
+        computerSelection = "paper";
     } else {
-        choice = "scissors";
+        computerSelection = "scissors";
     }
-    return choice;
+    return computerSelection;
 }
+
