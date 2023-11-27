b948ff9fae5c1ffe31c066e65dbd84ba7f3e0366
Edgar <EdgarEGarcia3@gmail.com>
2023-11-26 19:49:28 -0600

    create getComputerChoice function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-function getComputerChoice {
-    
+function getComputerChoice() {
+    let number = Math.floor(Math.random()*3);
+    let choice;
+    if (number == 0){
+        choice = "rock";
+    } else if (number == 1) {
+        choice = "paper";
+    } else {
+        choice = "scissors";
+    }
+    return choice;
 }
